497c9a3f66c1ea9d55ca1f953c42c59911a25e6d
Dave Lunny <dave@rithm.me>
2016-09-01 10:35:10 -0400

    Updates prompts to new promise-based version

--- utils/_prompts.js
+++ utils/_prompts.js
@@ -8,67 +8,42 @@ const generatorsList = pkg.files.filter(file => file !== 'app' && file !== 'base
 //  Each prompt is called using .apply which passes in the `this` context (reffering to the generator)
 const prompts = {
   language() {
-    return new Promise((res, rej) => {
-      this.prompt({
-        type: 'list',
-        name: 'language',
-        message: 'Do you want to use Sass or Less?',
-        choices: ['Sass', 'Less']
-      }, answer => {
-        if (!answer) rej(new Error('Error with language prompt!'));
-        res(answer.language);
-      });
+    return this.prompt({
+      type: 'list',
+      name: 'language',
+      message: 'Do you want to use Sass or Less?',
+      choices: ['Sass', 'Less']
     });
   },
   testing() {
-    return new Promise((res, rej) => {
-      this.prompt({
-        type: 'confirm',
-        name: 'testing',
-        message: 'Would you like to add visual regression testing, using CasperJS?',
-        default: false
-      }, answer => {
-        if (!answer) rej(new Error('Error with testing prompt!'));
-        res(answer.testing);
-      });
+    return this.prompt({
+      type: 'confirm',
+      name: 'testing',
+      message: 'Would you like to add visual regression testing, using CasperJS?',
+      default: false
     });
   },
   localConfigs() {
-    return new Promise((res, rej) => {
-      this.prompt({
-        type: 'confirm',
-        name: 'localConfigs',
-        message: 'Would you like to have local configs for your modules/units/pages etc. ?',
-        default: false
-      }, answer => {
-        if (!answer) rej(new Error('Error with localConfigs prompt!'));
-        res(answer.localConfigs);
-      });
+    return this.prompt({
+      type: 'confirm',
+      name: 'localConfigs',
+      message: 'Would you like to have local configs for your modules/units/pages etc. ?',
+      default: false
     });
   },
   listGenerators() {
-    return new Promise((res, rej) => {
-      this.prompt({
-        type: 'list',
-        name: 'generator',
-        message: 'Which generator would you like to run?',
-        choices: generatorsList
-      }, answer => {
-        if (!answer) rej(new Error('Error with listGenerators prompt!'));
-        res(answer);
-      });
+    return this.prompt({
+      type: 'list',
+      name: 'generator',
+      message: 'Which generator would you like to run?',
+      choices: generatorsList
     });
   },
   askName(type) {
-    return new Promise((res, rej) => {
-      this.prompt({
-        type: 'question',
-        name: 'name',
-        message: `What would you like to name this ${type}?`,
-      }, answer => {
-        if (!answer) rej(new Error('Error with askName prompt!'));
-        res(answer);
-      });
+    return this.prompt({
+      type: 'question',
+      name: 'name',
+      message: `What would you like to name this ${type}?`,
     });
   }
 };
